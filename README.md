b523a5359250a2a7af90be1891c771c955ad188f
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2022-04-09 15:23:03 +0200

    Added logrus

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 ## Purpose
 Execercies in go
 
+## FAQ
+### Howto install a module
+To install logrus
+
+```go
+go mod init
+go mod tidy
+```
+[Since go 1.17 go get no longer supported](https://go.dev/doc/go-get-install-deprecation)
+
+
 ## References
 - [go](https://go.dev/)
+- [logrus, logging framework](https://github.com/Sirupsen/logrus)
 - [setup environment](https://www.digitalocean.com/community/tutorials/how-to-install-go-and-set-up-a-local-programming-environment-on-ubuntu-18-04)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	//log.SetLevel(log.WarnLevel)
+	log.SetLevel(log.DebugLevel)
+
+}
 
 func main() {
 	fmt.Println("bank")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+
+	log.Info("This is a warning")
 }
