df08c656958a9d0dc9eec62a0bcb242e04215e08
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2022-04-23 16:32:37 +0200

    Reading database

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"math/rand"
 	"os"
-	"strconv"
 
 	"github.com/bank/domain"
+	"github.com/bank/test"
+
 	"github.com/jackc/pgx/v4/pgxpool"
 	log "github.com/sirupsen/logrus"
 )
@@ -56,100 +55,16 @@ func main() {
 
 	const maxnumber = 20
 
-	addAccounts(maxnumber)
-	accounts = readAccounts(maxnumber)
+	test.AddAccounts(dbpool, maxnumber)
+	accounts = test.ReadAccounts(dbpool, maxnumber)
 
-	addTargets(maxnumber)
-	addTransactions(maxnumber, accounts)
+	test.AddTargets(dbpool, maxnumber)
+	test.AddTransactions(dbpool, maxnumber, accounts)
 	log.Info("Closed  Bank")
 }
 
 func CheckError(err error) {
 	if err != nil {
 		panic(err)
-	}
-}
-
-func addAccounts(maxnumber int) {
-	log.Info("Add accounts")
-	account := domain.Account{}
-
-	account.SetNumber("1234567890")
-	account.SetDescription("none")
-	for i := 0; i < maxnumber; i++ {
-		str := strconv.Itoa(i)
-		account.SetId(0)
-		account.SetNumber(str)
-		id, err := account.Write(dbpool)
-		fmt.Printf("%v %T\n", account, account)
-
-		if err != nil {
-			log.Error("addAccounts: Error during insert account")
-		} else {
-			log.WithFields(log.Fields{"id": id}).Info("Added account")
-		}
-	}
-}
-
-func readAccounts(maxnumber int) []domain.Account {
-	var accounts []domain.Account
-	var account domain.Account
-
-	accounts, err := account.Read(dbpool, maxnumber)
-
-	if err == nil {
-		for _, account = range accounts {
-			fmt.Println("Found account: %v\n", account)
-		}
-	}
-
-	return accounts
-}
-
-func addTargets(maxnumber int) {
-	log.Info("Add targets")
-	target := domain.Target{}
-	target.SetName("Car")
-	target.SetDescription("none")
-
-	for i := 0; i < maxnumber; i++ {
-		str := "name: " + strconv.Itoa(i)
-		target.SetId(0)
-		target.SetName(str)
-
-		id, err := target.Write(dbpool)
-		fmt.Printf("%v %T\n", target, target)
-
-		if err != nil {
-			log.Error("addTargets: Error during insert target")
-		} else {
-			log.WithFields(log.Fields{"id": id}).Info("Added target")
-		}
-	}
-}
-
-func addTransactions(maxnumber int, accounts []domain.Account) {
-	log.Info("Add transactions")
-
-	transaction := domain.Transaction{}
-	transaction.SetTarget(1)
-
-	var i int
-	for i = 0; i < maxnumber; i++ {
-		str := "transaction: " + strconv.Itoa(i)
-		transaction.SetId(0)
-		transaction.SetDescription(str)
-		transaction.SetFromAccount(accounts[i].GetId())
-		transaction.SetToAccount(accounts[maxnumber-(i+1)].GetId())
-		transaction.SetAmount(int64(rand.Intn(25000)))
-
-		id, err := transaction.Write(dbpool)
-		fmt.Printf("%v %T\n", transaction, transaction)
-
-		if err != nil {
-			log.Error("addTransactions: Error during insert transaction")
-		} else {
-			log.WithFields(log.Fields{"id": id}).Info("Added transaction")
-		}
 	}
 }
---
+++ test/test.go
@@ -0,0 +1,95 @@
+package test
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+
+	"github.com/bank/domain"
+	"github.com/jackc/pgx/v4/pgxpool"
+	log "github.com/sirupsen/logrus"
+)
+
+func AddAccounts(dbpool *pgxpool.Pool, maxnumber int) {
+	log.Info("Add accounts")
+	account := domain.Account{}
+
+	account.SetNumber("1234567890")
+	account.SetDescription("none")
+	for i := 0; i < maxnumber; i++ {
+		str := strconv.Itoa(i)
+		account.SetId(0)
+		account.SetNumber(str)
+		id, err := account.Write(dbpool)
+		fmt.Printf("%v %T\n", account, account)
+
+		if err != nil {
+			log.Error("addAccounts: Error during insert account")
+		} else {
+			log.WithFields(log.Fields{"id": id}).Info("Added account")
+		}
+	}
+}
+
+func ReadAccounts(dbpool *pgxpool.Pool, maxnumber int) []domain.Account {
+	var accounts []domain.Account
+	var account domain.Account
+
+	accounts, err := account.Read(dbpool, maxnumber)
+
+	if err == nil {
+		for _, account = range accounts {
+			fmt.Println("Found account: %v\n", account)
+		}
+	}
+
+	return accounts
+}
+
+func AddTargets(dbpool *pgxpool.Pool, maxnumber int) {
+	log.Info("Add targets")
+	target := domain.Target{}
+	target.SetName("Car")
+	target.SetDescription("none")
+
+	for i := 0; i < maxnumber; i++ {
+		str := "name: " + strconv.Itoa(i)
+		target.SetId(0)
+		target.SetName(str)
+
+		id, err := target.Write(dbpool)
+		fmt.Printf("%v %T\n", target, target)
+
+		if err != nil {
+			log.Error("addTargets: Error during insert target")
+		} else {
+			log.WithFields(log.Fields{"id": id}).Info("Added target")
+		}
+	}
+}
+
+func AddTransactions(dbpool *pgxpool.Pool, maxnumber int, accounts []domain.Account) {
+	log.Info("Add transactions")
+
+	transaction := domain.Transaction{}
+	transaction.SetTarget(1)
+
+	var i int
+	for i = 0; i < maxnumber; i++ {
+		str := "transaction: " + strconv.Itoa(i)
+		transaction.SetId(0)
+		transaction.SetDescription(str)
+		transaction.SetFromAccount(accounts[i].GetId())
+		transaction.SetToAccount(accounts[maxnumber-(i+1)].GetId())
+		transaction.SetAmount(int64(rand.Intn(25000)))
+
+		id, err := transaction.Write(dbpool)
+		fmt.Printf("%v %T\n", transaction, transaction)
+
+		if err != nil {
+			log.Error("addTransactions: Error during insert transaction")
+		} else {
+			log.WithFields(log.Fields{"id": id}).Info("Added transaction")
+		}
+	}
+}
